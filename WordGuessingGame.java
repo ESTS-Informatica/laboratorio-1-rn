b5e376c10d7f35cdb864049b60a6fb2a6ffdf288
dannhypais <202200286@estudantes.ips.pt>
2024-04-05 16:26:47 +0100

    nivel2 incompleto
    falta o metodo play e acabar o guess, showResult

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -10,26 +10,45 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
     
     public WordGuessingGame(){
+        reader = new InputReader();
         hiddenWord = "abc";
         guessedWord = "___";
         numberOfTries = 0;
     }
     
-    private String getHiddenWord(){
+    public String getHiddenWord(){
         return hiddenWord;
     }
     
-    private String getGuessedWord(){
+    public String getGuessedWord(){
         return guessedWord;
     }
     
-    private int getNumberOfTries(){
+    public int getNumberOfTries(){
         return numberOfTries;
     }
     
     private void showGuessedWord(){
         System.out.println(guessedWord);
+    }
+    
+    public void play(){
+        
+    }
+    
+    private void showWelcome(){
+        System.out.println("Bem-Vindo ao jogo da forca!");
+    }
+    
+    private void guess(char guessedChar){
+        String aux="";
+        for(int i=0;i<hiddenWord.length();i++){
+            if(hiddenWord.charAt(i)==guessedChar){
+                aux = hiddenWord.substring(0,i) + guessedChar + hiddenWord.substring(i + 1);
+            }
+        }
     }
 }
